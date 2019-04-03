6a4e16f416dd67b9b98f9f124a27af4b816ada95
JBraumueller <jbraum@mit.edu>
2019-04-03 18:39:02 -0400

    changed the order in whcih cell size and box size is set: setting cell size now after box size, in order to avoid rounding errors in the cell size

--- sonpy.py
+++ sonpy.py
@@ -1400,10 +1400,11 @@ class sonnet(object):
         self.project.geo.lorgn = lorgn
 
         # Redefine the confining box (BOX)
-        self.project.geo.box.xwidth = xmax - xmin
-        self.project.geo.box.ywidth = ymax - ymin
         self.project.geo.box.xcells2 = int(2*(xmax - xmin)/xcellsize)
         self.project.geo.box.ycells2 = int(2*(ymax - ymin)/ycellsize)
+
+        self.project.geo.box.xwidth = int((xmax - xmin)/xcellsize)*xcellsize
+        self.project.geo.box.ywidth = int((ymax - ymin)/ycellsize)*ycellsize
 
         # Shift the circuit (all polygons, components and polygons)
         for dlayer in self.project.geo.dlayers:
